7c499783689242907ad77305ed9adb379a78d665
Tureika Valeria <ler4ik65@gmail.com>
2022-04-11 19:06:42 +0200

    the first task

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,26 @@
+"use strict";
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+    let arr;
+    let x = -b / (2 * a);
+    let x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    let x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+    let discriminant = b ** 2 - 4 * a * c;
+
+    if (discriminant < 0) {
+        arr = [];
+    } else if (discriminant == 0) {
+        arr = [x];
+    } else if (discriminant > 0) {
+        arr = [x1, x2];
+    }
+    return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
+// function calculateTotalMortgage(percent, contribution, amount, date) {
+//     let totalAmount;
 
-  // код для задачи №2 писать здесь
+//     // код для задачи №2 писать здесь
 
-  return totalAmount;
-}
+//     return totalAmount;
+// }
