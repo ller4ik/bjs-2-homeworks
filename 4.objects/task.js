b7955bf71504b2dc40736647be4313dca657f536
Tureika Valeria <ler4ik65@gmail.com>
2022-05-06 13:48:02 +0200

    first commit

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+};
 
-}
+Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
+};
 
-Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+Student.prototype.addMark = function(mark) {
+    if (this.marks === undefined) {
+        this.marks = [];
+        this.marks.push(mark);
+    } else {
+        this.marks.push(mark);
+    }
+};
 
-// ваш код для остальных методов
+Student.prototype.addMarks = function(...newmarks) {
+    if (this.marks === undefined) {
+        this.marks = [];
+        for (let arg of newmarks) {
+            this.marks.push(arg);
+        }
+    } else {
+        for (let arg of newmarks) {
+            this.marks.push(arg);
+        }
+    }
+};
+
+Student.prototype.getAverage = function() {
+    let sum = 0;
+    for (var i = 0; i < this.marks.length; ++i) {
+        sum += this.marks[i];
+        avg = Number((sum / this.marks.length));
+    }
+    return avg;
+};
+
+Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+};
